dab72c4f17a7acfc8778f9cf434413e938b1a985
VorobyevVladimir <vladimirv15012010@gmail.com>
2026-02-27 22:34:37 +0300

    add binary search

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,28 @@ public class Main {
     public static void main(String[] args){
 
     }
+
+    public static int binarySearch(int[] arr, int item) {
+        int low = 0;
+        int high = arr.length - 1;
+        int guess;
+
+        while (low <= high){
+            int mid = (low + high) / 2;
+            guess = arr[mid];
+            if (guess == item){
+                return mid;
+            } else if (guess > item) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+
+
+
 }
